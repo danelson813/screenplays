179ef73c632bcf294a4607b2201eb2bbd47ca1a1
Dan Nelson <danelson@nwi.net>
2022-11-10 23:59:02 -0800

    first working copy

--- main.py
+++ main.py
@@ -1,6 +1,27 @@
 #importing necessary packages
+import os
+import pandas as pd
+import convertapi  # converts pdfs to txt files
 import requests
 from bs4 import BeautifulSoup
-import os
-import convertapi #converts pdfs to txt files
 
+url = "https://thescriptsavant.com/movies.html"
+base = 'https://thescriptsavant.com'
+#scrapping titles and pdf links from script-savant (movies only A to M)
+
+req = requests.get(url)
+soup = BeautifulSoup(req.text, 'html.parser')
+
+#initializing dictionary to store title and pdf link to movie
+movie_lst = []
+title_count = 0
+for i in soup.find_all('a'):
+    if '/movies/' in i['href']:
+        link = base + i['href']
+        title = i.get_text()[:-7]
+        
+        result = {'title': title, "link": link }
+        movie_lst.append(result)
+    
+df = pd.DataFrame(movie_lst)   
+df.to_csv('Movie_List.csv', index=False)
